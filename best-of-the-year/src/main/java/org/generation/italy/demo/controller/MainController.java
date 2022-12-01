1aac4c45c6f95e2a961a95fdf457c8ad422f4a68
Marco Cafasso <marcocafassog@gmail.com>
2022-12-01 17:43:52 +0100

    feat: add a method that passes an attribute from the controller through the model
    method that returns a view made with Thymeleaf in which a title is printed: "Best of the year by ..." (instead of the dots, the name passed as an attribute from the controller through the Model must appear).

--- best-of-the-year/src/main/java/org/generation/italy/demo/controller/MainController.java
+++ best-of-the-year/src/main/java/org/generation/italy/demo/controller/MainController.java
@@ -1,14 +1,19 @@
 package org.generation.italy.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
 
 	@RequestMapping("/")
-	public String getHome() {
-		
+	public String getHome(Model model) {
+
+		String name = "markcaf";
+
+		model.addAttribute("name", name);
+
 		return "home"; 
 	}
 }
